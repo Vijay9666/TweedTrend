b7dfa156907ded930af60dda700e047d2913017e
EC2 Default User <ec2-user@ip-172-31-28-145.ec2.internal>
2023-08-07 15:15:34 +0000

    first commit

---
+++ ecommerce/asgi.py
@@ -0,0 +1,19 @@
+# pylint: disable=import-error
+# pylint: disable=no-name-in-module
+# pylint: disable=invalid-name
+"""
+ASGI config for ecommerce project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+
+application = get_asgi_application()
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ec2-user/environment/x22156411-TweedTrend/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
